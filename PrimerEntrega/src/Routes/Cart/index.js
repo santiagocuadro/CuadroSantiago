1f0692bcb67c1a0e176adc0d0e2c746f4f635a0b
santiagocuadro <santicua2000@gmail.com>
2022-11-02 18:04:52 -0300

    ultima modificacion de primer entrega

--- PrimerEntrega/src/Routes/Cart/index.js
+++ PrimerEntrega/src/Routes/Cart/index.js
@@ -5,12 +5,7 @@ import { DATE_UTILS } from "../../utils/index.js";
 
 const router = express.Router();
 
-router.get("", async (req, res) => {
-  const cart = await CarritoApi.getAll();
-  res.send({carrito: cart});
-});
-
-// Crea un carrito y devuelve su id
+// Crea un carrito y devuelve su id 
 router.post("/", async (req, res) => {
   try {
     const baseCart = { timestamp: DATE_UTILS.getTimestamp(), products: [] };
@@ -22,45 +17,79 @@ router.post("/", async (req, res) => {
   
 });
 
-// Vacia un carrito y lo elimina
+// Vacia un carrito y lo elimina 
 router.delete("/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    await CarritoApi.deleteAll();
+    await CarritoApi.deleteById(Number(id));
 
     res.send({ success: true });
   } catch (error) {
-    res.send({ success: false, error: error});
+    res.send({ success: false });
   }
 });
 
-// Me permite listar todos los productos guardados en el carrito
-router.get("/:id/productos", async (req, res) => {});
-
-// Para incorporar productos al carrito por su id de producto
-router.post("/:cartId/productos", async (req, res) => {
-  const { productId } = req.body;
-  const { cartId } = req.params;
-
-  const cart = await CarritoApi.getById(Number(cartId));
-
-  if (!cart)
-    return res.send({ error: true, message: ERRORS_UTILS.MESSAGES.NO_CART });
-
-  const product = await ProductosApi.getById(Number(productId));
-
-  if (!product)
-    return res.send({ error: true, message: ERRORS_UTILS.MESSAGES.NO_PRODUCT });
-
-  // TODO
-  cart.products.push(product);
-
-  const updatedCart = await CarritoApi.updateById(Number(cartId), cart);
-
-  res.send({ success: true, cart: updatedCart });
+// Me permite listar todos los productos guardados en el carrito 
+router.get("/:id/productos", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const cart = await CarritoApi.getById(Number(id));
+    const products = cart.products;
+    res.send({success: true, productos: products})
+  } catch (error) {
+    
+  }
 });
 
-// Elimina un producto del carrito por su id de carrito y de producto
-router.delete("/:id/productos/:id_prod", async (req, res) => {});
+// Para incorporar productos al carrito por su id de producto 
+router.post("/:id/productos", async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const { id } = req.params;
+    const cart = await CarritoApi.getById(Number(id));
+
+    if (!cart) {
+      return res.send({ success: false, message: "carrito vacio" });
+    }
+
+    const product = await ProductosApi.getById(Number(productId));
+
+    if (!product) {
+      return res.send({ success: false, message: "lista de productos vacia" });
+    }
+
+    cart.products.push(product);
+
+    const updatedCart = await CarritoApi.updateById(Number(id), cart);
+
+    res.send({ success: true, cart: updatedCart });
+  } catch (error) {
+    res.send({ success: false, cart: updatedCart });
+  }
+});
+
+// Elimina un producto del carrito por su id de carrito y de producto 
+router.delete("/:id/productos/:id_prod", async (req, res) => {
+  try {
+    const { id_prod } = req.params;
+    const { id } = req.params;
+    const cart = await CarritoApi.getById(Number(id));
+
+    if (!cart) {
+      return res.send({ success: false, message: "Carrito no encontrado" });
+    }
+
+    const index = cart.products.findIndex((element) => element.id === Number(id_prod));
+
+    if (index === -1) return res.send({success: false, message: "producto no encontrado"});
+
+    cart.products = cart.products.filter((element) => element.id !== Number(id_prod));
+    await CarritoApi.updateById(Number(id), cart);
+    
+    res.send({ success: true , result: cart.products});
+  } catch (error) {
+    res.send({ success: false });
+  }
+});
 
 export { router as routerCarrito };
--- PrimerEntrega/src/Routes/Product/index.js
+++ PrimerEntrega/src/Routes/Product/index.js
@@ -5,25 +5,20 @@ import {verifyRole} from "../../middlewares/verifyRole.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const product = await ProductosApi.getAll();
-
-    res.send(product);
-  } catch (error) {
-    res.send({ success: false });
-  }
-});
-
 
 // Me permite listar todos los productos disponibles o un producto por su id
 // (discponible para usuarios y administradores)
-router.get("/:id", async (req, res) => {
+router.get("/:id?", async (req, res) => { 
   try {
     const { id } = req.params;
-    const products = await ProductosApi.getById(Number(id));
 
-    res.send({products: products});
+    if(id !== undefined){
+      const products = await ProductosApi.getById(Number(id));  
+      res.send({products: products});
+    }else {
+      const products = await ProductosApi.getAll();  
+      res.send(products);
+    }
   } catch (error) {
     res.send({success: false});
   }
@@ -63,9 +58,13 @@ router.put("/:id", async (req, res) => {
 // Borra un producto por su id
 // (disponible para administradores)
 router.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  await ProductosApi.deleteById(Number(id));
-  res.send({ success: true });
+  try {
+    const { id } = req.params;
+    await ProductosApi.deleteById(Number(id));
+    res.send({ success: true });
+  } catch (error) {
+    res.send({ success: false });
+  }
 });
 
 export { router as routerProducts };
